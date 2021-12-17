d53896b1dd81fa73b09a3f389733545ea290be8d
songyeon <https://github.com/songyeon92>
2021-12-17 18:59:33 +0900

    ADD-Security 초안 작성

---
+++ src/main/java/com/sparta/weeklytestspring/security/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.sparta.weeklytestspring.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/")
+                .failureUrl("/")
+                .defaultSuccessUrl("/")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+}
